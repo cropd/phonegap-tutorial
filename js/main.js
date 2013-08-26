085844856511289b8cd6ac319dea73188c1a2b83
Eduard Klinger <eduard.klinger@gmail.com>
2013-08-26 23:22:21 +0200

    Add notifiation test

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ var app = {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
-            alert(title ? (title + ": " + message) : message);
+            alert('bla');
         }
     },
 
